e4a3fd27251c069861e27e11f389b5f662ad7aa2
NihadAliG <105681735+NihadAliG@users.noreply.github.com>
2022-06-07 11:04:20 +0200

    Add files via upload

---
+++ Marwa/src/test/java/OpenBrowser.java
@@ -0,0 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.close();
+        driver.quit();
+    }
+}
